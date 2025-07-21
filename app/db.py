8690ae5123f83a2daf8de90608e000d3bbc971c7
Feremos <mateuszrak2k@gmail.com>
2025-07-21 12:53:12 +0200

    added a static database connection

--- app/db.py
+++ app/db.py
@@ -1,8 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./emails.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Odczytaj DATABASE_URL ze zmiennych środowiskowych
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# PostgreSQL nie potrzebuje connect_args={"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
